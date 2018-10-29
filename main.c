d6a5a0dfbdaed62de1b054e3f5bed18919279c18
siroyan <1517053@ed.tus.ac.jp>
2018-10-29 15:41:04 +0900

    [add] cleaner sys

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 #define MAXLOOP 1000
 #define MANY 0
@@ -14,12 +15,14 @@ static int toilet[MAXLOOP];
 int main(int argc, char *argv[]){
 	int buf;
 	int time = 0;
+	int flag = 0;
+	int icln = 0;
 	double pp1 = 600;
 	double pp2 = 600;
 	double use_pp = 80;
+	double *pp_h, *pp_l;
 	int i, itemp, itemp2;
-	int cln_arr = 360;
-	double usr_arr = 15.0;
+	double usr_arr = 50.0;
 
 	itemp2 = 0;
 	do {
@@ -28,13 +31,64 @@ int main(int argc, char *argv[]){
 	} while (itemp2 < MAXLOOP && ( toilet[itemp2] = 1 ));
 
 	while(1){
-		if(toilet[time] == 1){
-			if(choose_pp == MANY) pp1 > pp2 ? pp1 - use_pp : pp2 - use_pp;
-			if(choose_pp ==  FEW) pp2 > pp1 ? pp1 - use_pp : pp2 - use_pp;
+		//Cleaner set pp.
+		if(icln == 60){
+			if(pp1 == 0) pp1 = 600;
+			if(pp2 == 0) pp2 = 600;
+			icln = 0;
 		}
-		
+
+		if(pp1 > pp2) {
+			pp_h = &pp1;
+			pp_l = &pp2;
+		} else {
+			pp_h = &pp2;
+			pp_l = &pp1;
+		}
+		if(toilet[time] == 1){
+			if(choose_pp() == MANY){
+				if(*pp_h > use_pp){
+					*pp_h -= use_pp;
+				}else{
+					if(*pp_l > use_pp){
+						*pp_l -= (use_pp - *pp_h);
+						*pp_h = 0;
+					}else{
+						*pp_l = 0;
+						*pp_h = 0;
+						printf("fuck\n");
+					}
+				}
+			}else{
+				if(*pp_l > use_pp){
+					*pp_l -= use_pp;
+				}else{
+					if(*pp_h > use_pp){
+						*pp_h -= (use_pp - *pp_l);
+						*pp_l = 0;
+					}else{
+						*pp_h = 0;
+						*pp_l = 0;
+						printf("fuck\n");
+					}
+				}
+			}
+		}
+
+		int i,j;
+		printf("pp1 [");
+		for(i=0;i<pp1;i=i+20){
+			printf("#");
+		}
+		printf("]\n");
+		printf("pp2 [");
+		for(j=0;j<pp2;j=j+20){
+			printf("#");
+		}
+		printf("]\n");
 
 		time++;
+		icln++;
 
 		printf("\nNext(Enter) or (Q)uit?");
 		scanf("%c", &buf);
@@ -46,15 +100,22 @@ int main(int argc, char *argv[]){
 double expdev(double ave){
 	double r;
 	double alpha;
-
 	alpha = 1/ave;
+	srand((unsigned int) time(NULL));
 	r = (float)rand()/(RAND_MAX + 1.0) * 1.0;
-	return -(1.0/alpha)*log(1.0-r) + 1.0;
+	return -(1.0/alpha)*log(1.0 - r) + 1.0;
 }
 
 int choose_pp(void){
 	double x;
+	srand((unsigned int) time(NULL));
 	x = (float)rand()/(RAND_MAX + 1.0) * 1.0;
-	if (x < 0.3) return MANY;
-	if (0.7 < x) return FEW;
+	if (x <= 0.0) {
+		printf("MANY\n");
+		return MANY;
+	}
+	if (0.0 < x) {
+		printf("FEW\n");
+		return FEW;
+	}
 }
