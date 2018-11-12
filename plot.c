a24c56686f5936148bc56a6a531c351dea8e06b6
siroyan <1517053@ed.tus.ac.jp>
2018-11-13 05:48:43 +0900

    [modify] refactoring

--- plot.c
+++ plot.c
@@ -6,7 +6,8 @@
 #define MAXLOOP 3600
 #define MANY 0
 #define FEW 1
-#define USR_ARR 30.0
+#define USEPP 80
+#define USR_ARR 100.0
 
 double expdev(double);
 int choose_pp(void);
@@ -17,45 +18,44 @@ static int oh;
 static double selectRatio = 0.0;
 
 int main(int argc, char *argv[]){
-	int pp1 = 600;
-	int pp2 = 600;
-	int use_pp = 80;
-	int *pp_h, *pp_l;
+	srand((unsigned) time(NULL));
+	int pp1, pp2, *pp_h, *pp_l;
+	int itemp, itemp2;
+	int time;
+	int cnt = 0;
 	FILE *fd;
-	fd = fopen("data.d","w");
+	fd = fopen("data100.d","w");
 	while(selectRatio < 1.0){
-		int time = 0;
+		pp1 = 600;
+		pp2 = 600;
+		time = 0;
+		itemp2 = 0;
 		int toilet[MAXLOOP] = {};
-		int itemp;
-		int itemp2 = 0;
 		do {
 			itemp = (int)(expdev(USR_ARR));
 			itemp2 += itemp;
 		} while (itemp2 < MAXLOOP && ( toilet[itemp2] = 1 ));
-
-		int icln = 0;
 		while(time < MAXLOOP){
-	    //Cleaner set pp.
-		    if(icln == 120){
-		      if(pp1 == 0) pp1 = 600;
-		      if(pp2 == 0) pp2 = 600;
-		      icln = 0;
+		    if(time % 120 == 0){
+		    	if(pp1 == 0) pp1 = 600;
+		    	if(pp2 == 0) pp2 = 600;
 		    }
 
 		    if(pp1 > pp2) {
-		      pp_h = &pp1;
-		      pp_l = &pp2;
-		    } else {
-		      pp_h = &pp2;
-		      pp_l = &pp1;
+		    	pp_h = &pp1;
+		    	pp_l = &pp2;
+		    }else{
+		    	pp_h = &pp2;
+		    	pp_l = &pp1;
 		    }
+
 			if(toilet[time] == 1){
 				if(choose_pp() == MANY){
-					if(*pp_h > use_pp){
-						*pp_h -= use_pp;
+					if(*pp_h > USEPP){
+						*pp_h -= USEPP;
 					}else{
-						if(*pp_l > use_pp){
-							*pp_l -= (use_pp - *pp_h);
+						if(*pp_l > USEPP){
+							*pp_l -= (USEPP - *pp_h);
 							*pp_h = 0;
 						}else{
 							*pp_l = 600;
@@ -64,11 +64,11 @@ int main(int argc, char *argv[]){
 						}
 					}
 				}else{
-					if(*pp_l > use_pp){
-						*pp_l -= use_pp;
+					if(*pp_l > USEPP){
+						*pp_l -= USEPP;
 					}else{
-						if(*pp_h > use_pp){
-							*pp_h -= (use_pp - *pp_l);
+						if(*pp_h > USEPP){
+							*pp_h -= (USEPP - *pp_l);
 							*pp_l = 0;
 						}else{
 							*pp_h = 600;
@@ -79,17 +79,12 @@ int main(int argc, char *argv[]){
 				}
 			}
 		    time++;
-		    icln++;
 		}
 		printf("--- R E S U L T ---\n");
-		printf("    MANY: %d\n", many);
-		printf("     FEW: %d\n", few);
-		printf("SELRATIO: %1.1f\n", selectRatio);
-		printf("   RATIO: %1.2f %%\n", (double) oh / (many + few) * 100);
+		printf(" MANY: %d\n", many);
+		printf("  FEW: %d\n", few);
+		printf("   Oh: %d\n", oh);
 		fprintf(fd, "%f %f\n", selectRatio * 100, (double) oh / (many + few) * 100);
-		icln = 0;
-		pp1 = 0;
-		pp2 = 0;
 		many = 0;
 		few = 0;
 		oh = 0;
